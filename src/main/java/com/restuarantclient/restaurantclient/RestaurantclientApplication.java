874845ee2bd39d3def9be3a1967120ed53b20b86
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-12-29 21:29:30 +0100

    Small refactor

--- src/main/java/com/restuarantclient/restaurantclient/RestaurantclientApplication.java
+++ src/main/java/com/restuarantclient/restaurantclient/RestaurantclientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantclientApplication {
+public class RestaurantClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestaurantclientApplication.class, args);
+        SpringApplication.run(RestaurantClientApplication.class, args);
     }
 
 }
